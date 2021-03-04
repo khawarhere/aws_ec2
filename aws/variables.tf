db4a9bc88c664e2c952f167cba7bec132a7452ba
khawarhere <khawarhere@gmail.com>
2021-03-05 04:56:20 +0500

    init

---
+++ aws/variables.tf
@@ -0,0 +1,15 @@
+variable "region" {
+  default = "us-east-1"
+}
+
+variable "instance_type" {
+  default = "t2.large"
+}
+
+variable "cluster_id" {
+  default = "rke"
+}
+
+variable "docker_install_url" {
+  default = "https://releases.rancher.com/install-docker/19.03.sh"
+}
